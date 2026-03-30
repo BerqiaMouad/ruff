d8464709fa092909c1af6c695735068747118c16
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2026-03-30 15:55:28 -0400

    infer lambda parameter types with `Callable` type context

--- crates/ty_python_semantic/src/types/ide_support.rs
+++ crates/ty_python_semantic/src/types/ide_support.rs
@@ -1837,9 +1837,8 @@ mod resolve_definition {
             | DefinitionKind::DictKeyAssignment(_)
             | DefinitionKind::For(_)
             | DefinitionKind::Comprehension(_)
-            | DefinitionKind::VariadicPositionalParameter(_)
-            | DefinitionKind::VariadicKeywordParameter(_)
             | DefinitionKind::Parameter(_)
+            | DefinitionKind::LambdaParameter { .. }
             | DefinitionKind::WithItem(_)
             | DefinitionKind::MatchPattern(_)
             | DefinitionKind::ExceptHandler(_)
